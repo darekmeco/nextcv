f89b4fcfb3bdc6e7e79993271fa7003dd368cd5b
Dariusz Męciński <dariusz.mecinski@gmail.com>
2019-12-12 23:06:41 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <a-layout id="components-layout-demo-responsive">
+    <a-layout-sider
+      @collapse="onCollapse"
+      @breakpoint="onBreakpoint"
+      breakpoint="lg"
+      collapsed-width="0"
+      width="300"
+    >
+      <a-card hoverable style="width: 240px">
+        <img
+          slot="cover"
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      </a-card>
+      <a-menu :defaultSelectedKeys="['4']" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <a-icon type="user" />
+          <span class="nav-text">nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera" />
+          <span class="nav-text">nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload" />
+          <span class="nav-text">nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <a-icon type="user" />
+          <span class="nav-text">nav 4</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-content :style="{ margin: '24px 16px 0' }">
+        <div
+          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
+        >
+          <nuxt />
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="textAlign: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<style></style>
