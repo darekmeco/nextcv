83c743b4b93b4bfdd28f5a63ed327e267ff1e0d9
darek <dariusz.mecinski@gmail.com>
2019-12-20 07:10:00 +0100

    cv init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <a-layout id="components-layout-demo-responsive">
-    <a-layout-sider breakpoint="lg" collapsed-width="0" width="300">
-      <a-avatar src="https://picsum.photos/id/237/200/300" />
-      <a-menu :defaultSelectedKeys="['4']" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <a-icon type="user" />
-          <span class="nav-text">nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <a-icon type="video-camera" />
-          <span class="nav-text">nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <a-icon type="upload" />
-          <span class="nav-text">nav 3</span>
-        </a-menu-item>
-        <a-menu-item key="4">
-          <a-icon type="user" />
-          <span class="nav-text">nav 4</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-
-    <a-layout>
-      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
-      <a-layout-content :style="{ margin: '24px 16px 0' }">
-        <div
-          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
-        >
-          <nuxt />
-        </div>
-      </a-layout-content>
-      <a-layout-footer style="textAlign: center">
-        Ant Design ©2018 Created by Ant UED
-      </a-layout-footer>
-    </a-layout>
-=======
   <a-layout id="components-layout-demo-top" class="layout">
     <a-layout-content
       :style="{ padding: '0 150px', width: '1200px', margin: '0 auto' }"
@@ -84,7 +46,6 @@
     <a-layout-footer style="text-align: center">
       NextCV ©2019 Created by D.
     </a-layout-footer>
->>>>>>> 9f614469cfafb28e5092c4f765fe560af360e729
   </a-layout>
 </template>
 <script>
