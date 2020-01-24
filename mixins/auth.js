94b527af6401ddc8618f25d87cd2ae6c48f8ae61
Dariusz Męciński <dariusz.mecinski@gmail.com>
2020-01-24 23:24:54 +0100

    auth clean

--- mixins/auth.js
+++ mixins/auth.js
@@ -18,31 +18,8 @@ class AuthMixin extends Vue {
    */
   logout() {
     this.$message.info("Logging out...");
-    console.log(this.$auth);
     this.$auth.$storage.setState("user", null);
-    // this.$auth.setToken(this.$state.strategy, false);
-    // this.$auth.setRefreshToken(this.$state.strategy, false);
     return Promise.resolve();
-    // await this.$auth.reset();
-    // await this.$auth
-    //   .logout()
-    //   .then(() => {
-    //     this.$message.success("Successfully disconnected");
-    //   })
-    //   .catch(err => {
-    //     this.$message.error("Error while disconnecting: " + err.message);
-    //   });
-
-    // If you are not fond of using axios promises on async calls
-    // You can still use Javascript try and catch block
-    //
-    // try {
-    //   this.$toast.show('Logging out...')
-    //   await this.$auth.logout()
-    //   this.$toast.success('Successfully disconnected')
-    // } catch (err) {
-    //   this.$toast.error('Error while disconnecting: ' + err.message)
-    // }
   }
 
   /**
@@ -65,7 +42,6 @@ class AuthMixin extends Vue {
         this.$message.success("Successfully connected");
       })
       .catch(err => {
-        console.log(err);
         this.$message.error("Please provide your username or your e-mail");
         this.loginHasError = true;
       });
